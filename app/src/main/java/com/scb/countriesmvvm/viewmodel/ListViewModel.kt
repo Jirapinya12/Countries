bdd51460c133cd9334401a84371461cbb0debf83
Jirpinya Eamsangchanchai <jirapinya.eamsangchanchai@scb.co.th>
2022-04-01 14:32:34 +0700

    create layout

--- app/src/main/java/com/scb/countriesmvvm/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/scb/countriesmvvm/viewmodel/ListViewModel.kt
@@ -1,6 +1,28 @@
 package com.scb.countriesmvvm.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.scb.countriesmvvm.Country
 
 class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"))
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
